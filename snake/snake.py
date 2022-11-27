f5a09e5025fd6a63e6d9da6bf876ce5ca9b85b82
kilinkarov <114779296+kilinkarov@users.noreply.github.com>
2022-11-27 23:48:34 +0300

    Update snake.py

--- snake/snake.py
+++ snake/snake.py
@@ -1,22 +1,95 @@
-
+from copy import deepcopy
+from numpy import sign
 import pygame
 import pygame_menu
-import numpy
 from random import randrange
-import copy
 from time import sleep
 
 RES = 800
 SIZE = 50
+ffps = 50
 fps = 60
 fps_old = 60
 fps_water = (3 * fps) // 2
 fps_sand = (2 * fps) // 3
 count = 0
-time = -5000
+max_time = 5000
+time = - max_time
+count_splitt = 10
+count_game = 0
+table_record = []
+score = 0
+flag = False
+flag_end = False
+flag_snowflake = False
+snowflake_xx = 1000
+
+
+Rock1 = ()
+Rock2 = ([3 * SIZE, 5 * SIZE], [9 * SIZE, 1 * SIZE], [11 * SIZE, 12 * SIZE])
+Rock3 = Rock2 + tuple([i * SIZE, 7 * SIZE] for i in range(2, 9)) + tuple([i * SIZE, 5 * SIZE] for i in range(13, 16))\
+        + tuple([i * SIZE, 6 * SIZE] for i in range(13, 16))
+Rock4 = tuple([7 * SIZE, i * SIZE] for i in range(6)) + tuple([7 * SIZE, i * SIZE] for i in range(9, 16)) + Rock2
+Rock5 = tuple([i * SIZE, 6 * SIZE] for i in range(1, 9)) + tuple([12 * SIZE, i * SIZE] for i in range(4, 13))\
+        + tuple([i * SIZE, 10 * SIZE] for i in range(1, 9)) + ([2 * SIZE, 2 * SIZE], [4 * SIZE, 13 * SIZE])
+lavel = {1: Rock1, 2: Rock2, 3: Rock3, 4: Rock4, 5: Rock5}
+
+Sand1 = ()
+Sand2 = ([4 * SIZE, 6 * SIZE], [12 * SIZE, 4 * SIZE], [5 * SIZE, 12 * SIZE])
+Sand3 = tuple(([(i + 1) * SIZE, i * SIZE] for i in range(2, 7))) + \
+        tuple(([(i + 2) * SIZE, i * SIZE] for i in range(2, 7)))
+Sand4 = ([4 * SIZE, 6 * SIZE], [12 * SIZE, 4 * SIZE], [5 * SIZE, 12 * SIZE])
+Sand5 = ([15 * SIZE, 0], [14 * SIZE, 0], [15 * SIZE, 1 * SIZE], [15 * SIZE, 15 * SIZE], [15 * SIZE, 14 * SIZE],
+         [14 * SIZE, 15 * SIZE]) + ([9 * SIZE, 6 * SIZE], [8 * SIZE, 5 * SIZE], [8 * SIZE, 7 * SIZE],
+                                    [9 * SIZE, 10 * SIZE], [8 * SIZE, 9 * SIZE], [8 * SIZE, 11 * SIZE])
+lavel_sand = {1: Sand1, 2: Sand2, 3: Sand3, 4: Sand4, 5: Sand5}
+Water1 = ()
+Water2 = ([7 * SIZE, 1 * SIZE], [13 * SIZE, 7 * SIZE], [2 * SIZE, 7 * SIZE])
+Water3 = tuple([i * SIZE, 13 * SIZE] for i in range(7)) + tuple([i * SIZE, 14 * SIZE] for i in range(8))\
+         + tuple([i * SIZE, 15 * SIZE] for i in range(9))
+Water4 = ([5 * SIZE, 1 * SIZE], [13 * SIZE, 7 * SIZE], [2 * SIZE, 7 * SIZE])
+Water5 = tuple([13 * SIZE, i * SIZE] for i in range(4, 13)) + tuple([14 * SIZE, i * SIZE] for i in range(5, 12))\
+         + tuple([15 * SIZE, i * SIZE] for i in range(7, 10))
+lavel_water = {1: Water1, 2: Water2, 3: Water3, 4: Water4, 5: Water5}
+
+pygame.init()
+surface = pygame.display.set_mode([RES, RES])
+surface_table = pygame.display.set_mode([RES, RES])
+clock = pygame.time.Clock()
+
+font_score = pygame.font.SysFont('New Times Roman', 45, bold=True)
+font_table = pygame.font.SysFont('New Times Roman', 70, bold=True)
+
+img = pygame.image.load('images.jpeg').convert()
+img = pygame.transform.scale(img, (RES, RES))
+img_table = pygame.image.load('02-e3e62bae.png')
+image_rock = pygame.image.load('rock.png')
+image_rock = pygame.transform.scale(image_rock, (SIZE, SIZE))
+image_sand = pygame.image.load('sand.png')
+image_sand = pygame.transform.scale(image_sand, (SIZE, SIZE))
+image_water = pygame.image.load('water.png')
+image_water = pygame.transform.scale(image_water, (SIZE, SIZE))
+apple_image = pygame.image.load('02-e3e62bae.png')
+apple_image = pygame.transform.scale(apple_image, (SIZE, SIZE))
+
+snowflake_image = pygame.image.load('snejinka.png')
+snowflake_image = pygame.transform.scale(snowflake_image, (SIZE, SIZE))
+
+x_apple = randrange(SIZE, RES - SIZE, SIZE)
+y_apple = randrange(SIZE, RES - SIZE, SIZE)
+
+x_snowflake = randrange(SIZE, RES - SIZE, SIZE)
+y_snowflake = randrange(SIZE, RES - SIZE, SIZE)
+
+difficult = 1
 
 
 class SNAKE:
+    count_split = 10
+    dx_now = 0
+    dy_now = 0
+    flag_end = False
+
     def __init__(self, x, y, rock, sand, water, img_rock, img_sand, img_water):
         self.x = x
         self.y = y
@@ -27,12 +100,7 @@ class SNAKE:
         self.image_sand = img_sand
         self.image_water = img_water
         self.snake = [[x, y]]
-        self.snake_now = copy.deepcopy(self.snake)
-
-    count_split = 10
-    dx_now = 0
-    dy_now = 0
-    flag_end = False
+        self.snake_now = deepcopy(self.snake)
 
     def draw_rock(self):
         global surface
@@ -46,12 +114,12 @@ class SNAKE:
     def move_snake(self, size, dx, dy):
         global count
         if count == 0:
-            self.dx_now = copy.deepcopy(dx)
-            self.dy_now = copy.deepcopy(dy)
-            self.snake_now = copy.deepcopy(self.snake)
+            self.dx_now = deepcopy(dx)
+            self.dy_now = deepcopy(dy)
+            self.snake_now = deepcopy(self.snake)
         for i in range(len(self.snake) - 1):
-            self.snake[i][0] += -numpy.sign(self.snake[i][0] - self.snake_now[i + 1][0]) * (size // self.count_split)
-            self.snake[i][1] += -numpy.sign(self.snake[i][1] - self.snake_now[i + 1][1]) * (size // self.count_split)
+            self.snake[i][0] += -sign(self.snake[i][0] - self.snake_now[i + 1][0]) * (size // self.count_split)
+            self.snake[i][1] += -sign(self.snake[i][1] - self.snake_now[i + 1][1]) * (size // self.count_split)
         self.snake[-1][0] += self.dx_now * (SIZE // self.count_split)
         self.snake[-1][1] += self.dy_now * (SIZE // self.count_split)
 
@@ -125,8 +193,8 @@ class APPLE:
 
     def crash(self, flagg):
         if flagg:
-            self.x = 1000
-            self.y = 1000
+            self.x = snowflake_xx
+            self.y = snowflake_xx
 
     def eating_apple(self, flagg, snake, size):
         if flagg:
@@ -185,78 +253,11 @@ class BUTTON:
                 self.buttons = {'W': 1, 'S': 1, 'A': 0, 'D': 1, }
 
 
-count_game = 0
-table_record = []
-score = 0
-flag = False
-flag_end = False
-flag_snowflake = False
-
-
-Rock1 = ()
-Rock2 = ([3 * SIZE, 5 * SIZE], [9 * SIZE, 1 * SIZE], [11 * SIZE, 12 * SIZE])
-Rock3 = Rock2 + tuple([i * SIZE, 7 * SIZE] for i in range(2, 9)) + tuple([i * SIZE, 5 * SIZE] for i in range(13, 16))\
-        + tuple([i * SIZE, 6 * SIZE] for i in range(13, 16))
-Rock4 = tuple([7 * SIZE, i * SIZE] for i in range(6)) + tuple([7 * SIZE, i * SIZE] for i in range(9, 16)) + Rock2
-Rock5 = tuple([i * SIZE, 6 * SIZE] for i in range(1, 9)) + tuple([12 * SIZE, i * SIZE] for i in range(4, 13))\
-        + tuple([i * SIZE, 10 * SIZE] for i in range(1, 9)) + ([2 * SIZE, 2 * SIZE], [4 * SIZE, 13 * SIZE])
-lavel = {1: Rock1, 2: Rock2, 3: Rock3, 4: Rock4, 5: Rock5}
-
-Sand1 = ()
-Sand2 = ([4 * SIZE, 6 * SIZE], [12 * SIZE, 4 * SIZE], [5 * SIZE, 12 * SIZE])
-Sand3 = tuple(([(i + 1) * SIZE, i * SIZE] for i in range(2, 7))) + \
-        tuple(([(i + 2) * SIZE, i * SIZE] for i in range(2, 7)))
-Sand4 = ([4 * SIZE, 6 * SIZE], [12 * SIZE, 4 * SIZE], [5 * SIZE, 12 * SIZE])
-Sand5 = ([15 * SIZE, 0], [14 * SIZE, 0], [15 * SIZE, 1 * SIZE], [15 * SIZE, 15 * SIZE], [15 * SIZE, 14 * SIZE],
-         [14 * SIZE, 15 * SIZE]) + ([9 * SIZE, 6 * SIZE], [8 * SIZE, 5 * SIZE], [8 * SIZE, 7 * SIZE],
-                                    [9 * SIZE, 10 * SIZE], [8 * SIZE, 9 * SIZE], [8 * SIZE, 11 * SIZE])
-lavel_sand = {1: Sand1, 2: Sand2, 3: Sand3, 4: Sand4, 5: Sand5}
-Water1 = ()
-Water2 = ([7 * SIZE, 1 * SIZE], [13 * SIZE, 7 * SIZE], [2 * SIZE, 7 * SIZE])
-Water3 = tuple([i * SIZE, 13 * SIZE] for i in range(7)) + tuple([i * SIZE, 14 * SIZE] for i in range(8))\
-         + tuple([i * SIZE, 15 * SIZE] for i in range(9))
-Water4 = ([5 * SIZE, 1 * SIZE], [13 * SIZE, 7 * SIZE], [2 * SIZE, 7 * SIZE])
-Water5 = tuple([13 * SIZE, i * SIZE] for i in range(4, 13)) + tuple([14 * SIZE, i * SIZE] for i in range(5, 12))\
-         + tuple([15 * SIZE, i * SIZE] for i in range(7, 10))
-lavel_water = {1: Water1, 2: Water2, 3: Water3, 4: Water4, 5: Water5}
-
-pygame.init()
-surface = pygame.display.set_mode([RES, RES])
-surface_table = pygame.display.set_mode([RES, RES])
-clock = pygame.time.Clock()
-
-font_score = pygame.font.SysFont('New Times Roman', 45, bold=True)
-font_table = pygame.font.SysFont('New Times Roman', 70, bold=True)
-
-img = pygame.image.load('images.jpeg').convert()
-img = pygame.transform.scale(img, (RES, RES))
-img_table = pygame.image.load('02-e3e62bae.png')
-image_rock = pygame.image.load('rock.png')
-image_rock = pygame.transform.scale(image_rock, (SIZE, SIZE))
-image_sand = pygame.image.load('sand.png')
-image_sand = pygame.transform.scale(image_sand, (SIZE, SIZE))
-image_water = pygame.image.load('water.png')
-image_water = pygame.transform.scale(image_water, (SIZE, SIZE))
-apple_image = pygame.image.load('02-e3e62bae.png')
-apple_image = pygame.transform.scale(apple_image, (SIZE, SIZE))
-
-snowflake_image = pygame.image.load('snejinka.png')
-snowflake_image = pygame.transform.scale(snowflake_image, (50, 50))
-
-x_apple = randrange(SIZE, RES - SIZE, SIZE)
-y_apple = randrange(SIZE, RES - SIZE, SIZE)
-
-x_snowflake = randrange(SIZE, RES - SIZE, SIZE)
-y_snowflake = randrange(SIZE, RES - SIZE, SIZE)
-
-difficult = 1
-
-
 def set_difficulty(value, difficulty):
-    global difficult, fps, fps_old, fps_water, fps_sand
+    global difficult, fps, fps_old, fps_water, fps_sand, ffps, count_splitt
     difficult = difficulty
-    fps = 50 + difficult * 10
-    fps_old = 50 + difficult * 10
+    fps = ffps + difficult * count_splitt
+    fps_old = ffps + difficult * count_splitt
     fps_water = fps * 1.5
     fps_sand = fps // 1.5
     pass
@@ -279,19 +280,19 @@ def table_records():
     for i in range(min(count_game, 5)):
         record = str(table_record[i][0]) + ':' + str(table_record[i][1])
         render_table = font_table.render(f' {record}', True, pygame.Color('black'))
-        surface_table.blit(render_table, (50, 100 + 100 * i))
+        surface_table.blit(render_table, (SIZE, 2 * SIZE + 2 * SIZE * i))
     sleep(5)
 
 
 def start_the_game():
     global score, count, fps, SIZE, RES, flag, surface,\
-        flag_end, flag_snowflake, time, count_game, fps_old, fps_water, fps_sand
+        flag_end, flag_snowflake, time, count_game, fps_old, fps_water, fps_sand, snowflake_xx, max_time
     apple = APPLE(x_apple, y_apple, apple_image, lavel[difficult], lavel_sand[difficult],
-                  lavel_water[difficult], 1000, 1000)
-    apple.eating_apple(True, [[750, 750]], SIZE)
-    snowflake = APPLE(1000, 1000, snowflake_image, lavel[difficult],
+                  lavel_water[difficult], snowflake_xx, snowflake_xx)
+    apple.eating_apple(True, [[RES - SIZE, RES - SIZE]], SIZE)
+    snowflake = APPLE(snowflake_xx, snowflake_xx, snowflake_image, lavel[difficult],
                       lavel_sand[difficult], lavel_water[difficult], apple.x, apple.y)
-    snake = SNAKE(750, 750, lavel[difficult], lavel_sand[difficult],
+    snake = SNAKE(RES - SIZE, RES - SIZE, lavel[difficult], lavel_sand[difficult],
                   lavel_water[difficult], image_rock, image_sand, image_water)
     button = BUTTON()
 
@@ -312,8 +313,8 @@ def start_the_game():
                 snake.snake[-1][1] + SIZE - 1 >= apple.y >= snake.snake[-1][1] - SIZE + 1):
             if count == 0:
                 score += 1
-                fps += 5
-                fps_old += 5
+                fps += count_splitt // 2
+                fps_old += count_splitt // 2
                 fps_water = 3 * fps_old // 2
                 fps_sand = 2 * fps_old // 3
                 flag = True
@@ -322,14 +323,14 @@ def start_the_game():
                 snake.snake[-1][1] + SIZE - 1 >= snowflake.y >= snake.snake[-1][1] - SIZE + 1):
             if count == 0:
                 if fps > 30:
-                    fps -= max(fps // 3, 30)
-                    fps_old = max(fps_old // 3, 30)
+                    fps -= fps // 3
+                    fps_old -= fps_old // 3
                 fps_water = (3 * fps_old) // 2
                 fps_sand = (2 * fps_old) // 3
                 score = max(0, score - 3)
                 flag_snowflake = True
                 snowflake.crash(flag_snowflake)
-                apple.chanche(1000, 1000)
+                apple.chanche(snowflake_xx, snowflake_xx)
                 time = 0
         snake.scaling_snake(SIZE)
         snake.move_snake(SIZE, button.dx, button.dy)
@@ -340,9 +341,9 @@ def start_the_game():
         count += 1
         time += 1
         snake.check_spand_or_water(SIZE)
-        if count == 9:
+        if count == (count_splitt - 1):
             count = 0
-        if time >= 5000:
+        if time >= max_time:
             snowflake.eating_apple(flag_snowflake, snake.snake, SIZE)
             apple.chanche(snowflake.x, snowflake.y)
             flag_snowflake = False
@@ -358,16 +359,15 @@ def start_the_game():
     make_string_table(name_input)
     table_records_append(make_string_table(name_input))
     fps = fps_old
-    fps = 50 + difficult * 10
-    fps_old = 50 + difficult * 10
+    fps = ffps + difficult * count_splitt
+    fps_old = ffps + difficult * count_splitt
     fps_water = fps_old * 1.5
     fps_sand = fps_old // 1.5
     score = 0
-    time = -5000
+    time = -max_time
 
 
 font = pygame_menu.font.FONT_DIGITAL
-
 mytheme = pygame_menu.Theme(
                 background_color=(0, 0, 0, 0),
                 widget_font=font,
